b59bf1e28ca3d98dac2672fd230ce18dba125e2f
Gabriel Coelho <coelhogabriel67@gmail.com>
2022-03-30 21:35:35 -0300

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>Qual CEP você deseja pesquisar ?</p>
+          <input type="number" />
+          <button>CONSULTAR</button>
+        </header>
+      </div>
+    </>
   );
 }
 
