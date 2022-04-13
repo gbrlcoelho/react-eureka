b324b5d90d7b29f4b8a29367ac4cbf2b16ba5d1c
Gabriel Coelho <coelhogabriel67@gmail.com>
2022-04-13 13:58:08 -0300

    Add mask and handleSearch

--- src/Containers/Pesquisa.tsx
+++ src/Containers/Pesquisa.tsx
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import InputMask from "react-input-mask";
+import consultarCep from "cep-promise";
+
+function numbersOnly(str: string) {
+  return str.replace(/[^\d]/g, "");
+}
 
 export function Pesquisa() {
-  const [cepNumber, setCepNumber] = useState<number>(0);
+  const [cepNumber, setCepNumber] = useState<string>("");
+  function handleSuccess(dadosCEP: number) {}
+
+  function handleError(err: string) {}
+
+  function handleSearch() {
+    consultarCep(cepNumber).then(handleSuccess).catch(handleError);
+    <Link to={"/result"}></Link>;
+  }
 
   return (
     <>
       <p>Qual CEP você deseja pesquisar?</p>
-      <input
+      <InputMask
+        mask="99999-999"
         value={cepNumber}
-        onChange={(event) => setCepNumber(parseInt(event.target.value))}
-        type="number"
+        onChange={(event) => setCepNumber(numbersOnly(event.target.value))}
         required
         placeholder="00000-000"
       />
-      <Link to={"/loading"}>
-        <button>CONSULTAR</button>
+      <Link to={"/result"}>
+        <button onClick={handleSearch}>CONSULTAR</button>
       </Link>
     </>
   );
